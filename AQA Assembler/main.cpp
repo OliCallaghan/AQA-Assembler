c466f5355ff48afd5da1090ab323d8e288fdf200
OliCallaghan <OliCallaghan@icloud.com>
2017-03-02 16:51:53 +0000

    Initial Commit

---
+++ AQA Assembler/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AQA Assembler
+//
+//  Created by Oli Callaghan on 02/03/2017.
+//  Copyright © 2017 Oli Callaghan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
