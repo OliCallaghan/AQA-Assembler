5671b6657e9ed999d529e1f34a30a4f2c2417cf4
OliCallaghan <OliCallaghan@icloud.com>
2017-03-04 00:28:51 +0000

    Tidies up renderer.js

--- AQA Assembler/renderer.js
+++ AQA Assembler/renderer.js
@@ -1,35 +1,46 @@
-// Render Process
-const {ipcRenderer} = require('electron')
+// Import Native NodeJS Libraries
 const path = require('path');
 
+// Import Electron Dependencies
+const {ipcRenderer} = require('electron')
+
+// Working File
 var wrking_file;
 
-wrking_file = '/Users/Oli/Documents/CRGS/Computer Science Society/AQA Assembler/AQA Assembler/test.asm';
-
+// Prompt user for file to open on launch
 ipcRenderer.send('selectfile', '');
 
+// User selected file
 ipcRenderer.on('selectedfile', (event, arg) => {
-	console.log(arg[0]);
-	document.getElementById('filename').innerHTML = path.basename(arg[0]);
-	ipcRenderer.send('open', arg[0]);
+    document.getElementById('filename').innerHTML = path.basename(arg[0]);
+    ipcRenderer.send('open', arg[0]);
 });
 
+// User opened file
 ipcRenderer.on('opened', (event, arg) => {
-	document.getElementById('code').value = arg;
+    // Set textarea contents to opened file
+    document.getElementById('code').value = arg;
 });
 
+// Real-time register data recieved
 ipcRenderer.on('registers', (event, arg) => {
-	var registers = document.getElementsByClassName("register");
-	for (register in registers) {
-		console.log(registers[register].childNodes);
-		if (registers[register].childNodes) {
-			registers[register].childNodes[2].innerHTML = arg[register];
-		}
-	}
+    // Update GUI with this datas
+    var registers = document.getElementsByClassName("register");
+    for (register in registers) {
+        if (registers[register].childNodes) {
+            registers[register].childNodes[2].innerHTML = arg[register];
+        }
+    }
 });
 
-document.getElementById('run').onclick = function () {
-	if (ipcRenderer.sendSync('save', {path: wrking_file, data: document.getElementById('code').value})) {
-		ipcRenderer.send('run', wrking_file);
-	}
+// Add handler for run button
+document.getElementById('run').onclick = function() {
+    // Save file first
+    if (ipcRenderer.sendSync('save', {
+            path: wrking_file,
+            data: document.getElementById('code').value
+        })) {
+        // Run file
+        ipcRenderer.send('run', wrking_file);
+    }
 }
